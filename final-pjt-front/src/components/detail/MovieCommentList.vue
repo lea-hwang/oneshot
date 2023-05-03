97daf9c4fa83d4f7d91593ad0e082dc26420e41c
lea-hwang <huiwon1488@gmail.com>
2023-05-03 16:32:15 +0900

    comment 마진 변경

--- final-pjt-front/src/components/detail/MovieCommentList.vue
+++ final-pjt-front/src/components/detail/MovieCommentList.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="py-5 px-md-5 comment-box mt-lg-5">
+  <div class="py-5 px-md-5 comment-box">
     <div class="comment-title-box">
       <div class="point-bar"></div>
-      <h4 class="comment-title">  COMMENTS</h4>
+      <h4 class="comment-title">COMMENTS</h4>
     </div>
     <div class="px-lg-5 py-3">
       <movie-comment-form></movie-comment-form>
@@ -10,7 +10,7 @@
         <div v-if="!movieComments.length">
           아직 작성된 코멘트가 없습니다! 코멘트를 남겨주세요 :)
         </div>
-        <movie-comment 
+        <movie-comment
           v-for="(comment, index) in movieComments"
           :key="index"
           :comment="comment"
@@ -22,47 +22,48 @@
 </template>
 
 <script>
-import MovieCommentForm from '@/components/detail/MovieCommentForm.vue'
-import MovieComment from '@/components/detail/MovieComment.vue'
-import { mapGetters } from 'vuex'
+import MovieCommentForm from "@/components/detail/MovieCommentForm.vue";
+import MovieComment from "@/components/detail/MovieComment.vue";
+import { mapGetters } from "vuex";
 
 export default {
-  name:'MovieCommentList',
+  name: "MovieCommentList",
   components: { MovieCommentForm, MovieComment },
   computed: {
-    ...mapGetters(['movieComments']),    
+    ...mapGetters(["movieComments"]),
   },
-}
+};
 </script>
 
 <style>
-  .comment-list {
-    width: 20%;
-    display: inline-block;
-  }
-  .point-bar {
-    display: inline-block;
-    width: 7px;
-    height: 30px;
-    background-color: #6E30F2;
-  }
-  .comment-title-box {
-    display: flex;
-    align-content: center;
-    margin-bottom: 20px;
-  }
-  .comment-title {
-    display: inline;
-    margin: 0 10px 0;
-    font-size: 24px;
-  }
+.comment-list {
+  width: 20%;
+  display: inline-block;
+}
+.point-bar {
+  display: inline-block;
+  width: 7px;
+  height: 30px;
+  background-color: #6e30f2;
+}
+.comment-title-box {
+  display: flex;
+  align-content: center;
+  margin-bottom: 20px;
+}
+.comment-title {
+  display: inline;
+  margin: 0 10px 0;
+  font-size: 24px;
+}
+.comment-box {
+  margin: auto;
+  width: 100%;
+  margin-top: 100px;
+}
+@media (min-width: 992px) {
   .comment-box {
-    margin: auto;
-    width: 100%;
+    width: 75%;
   }
-   @media (min-width: 992px) {
-    .comment-box {
-      width: 75%;
-    }
-  }
+}
 </style>
