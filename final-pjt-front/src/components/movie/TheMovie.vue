5d20fa631da8840b3ed92e9a572a59fac24c3e83
lea-hwang <huiwon1488@gmail.com>
2023-05-03 15:37:09 +0900

    shot 위에 cursor pointer로 수정

--- final-pjt-front/src/components/movie/TheMovie.vue
+++ final-pjt-front/src/components/movie/TheMovie.vue
@@ -1,13 +1,28 @@
 <template>
-  <div @click="clickMovie(movie.pk)" class="col-xl-3 col-lg-4 col-md-6 col-12 mb-3">
+  <div
+    @click="clickMovie(movie.pk)"
+    class="col-xl-3 col-lg-4 col-md-6 col-12 mb-3 card-body"
+  >
     <div class="position-relative h-100">
-      <img :src="moviePosterPath" alt="poster image" class="w-100 h-100" @mouseenter="mouseEnter" >
-      <div :class="{ 'movie-info-block': active }" 
-        class="w-100 h-100 d-flex flex-column justify-content-center" 
-        @mouseleave="mouseLeave">
+      <img
+        :src="moviePosterPath"
+        alt="poster image"
+        class="w-100 h-100"
+        @mouseenter="mouseEnter"
+      />
+      <div
+        :class="{ 'movie-info-block': active }"
+        class="w-100 h-100 d-flex flex-column justify-content-center"
+        @mouseleave="mouseLeave"
+      >
         <h5 v-show="active" class="fw-bolder">{{ movie.title }}</h5>
         <div>
-          <span v-show="active" v-for="(genre, index) in movie.genres" :key="index">{{ genre.name }} </span>
+          <span
+            v-show="active"
+            v-for="(genre, index) in movie.genres"
+            :key="index"
+            >{{ genre.name }}
+          </span>
         </div>
       </div>
     </div>
@@ -16,48 +31,47 @@
 
 <script>
 export default {
-  name: 'TheMovie',
-  data(){
+  name: "TheMovie",
+  data() {
     return {
       active: false,
-    }
+    };
   },
-  props:{
+  props: {
     movie: Object,
   },
   computed: {
     moviePosterPath() {
-      return `https://image.tmdb.org/t/p/original/${this.movie.poster_path}`
+      return `https://image.tmdb.org/t/p/original/${this.movie.poster_path}`;
     },
   },
-  methods:{
+  methods: {
     clickMovie(moviePk) {
-      this.$router.push({ name: 'detail', params: { movieId : moviePk } })
+      this.$router.push({ name: "detail", params: { movieId: moviePk } });
     },
-    mouseEnter(){
-      this.active = true
+    mouseEnter() {
+      this.active = true;
     },
-    mouseLeave(){
-      this.active = false
+    mouseLeave() {
+      this.active = false;
     },
   },
-}
+};
 </script>
 
 <style scoped>
-  .card-body {
-    padding-bottom: 0;
-  }
+.card-body {
+  cursor: pointer;
+}
 
-  .movie-info-block {
-    background: black;
-    width: 100px;
-    height: 100px;
-    position: absolute;
-    top: 0;
-    opacity: 0.7;
-    color: white;
-    text-align: center;
-  }
-
+.movie-info-block {
+  background: black;
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: 0;
+  opacity: 0.7;
+  color: white;
+  text-align: center;
+}
 </style>
