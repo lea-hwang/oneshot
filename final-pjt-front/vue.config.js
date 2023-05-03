395bcb885a61b92d73fbc680959cecbdaad76f72
lea-hwang <huiwon1488@gmail.com>
2023-05-03 15:35:21 +0900

    vue.config 수정해서 템플릿 문법에 ES2015 적용하기

--- final-pjt-front/vue.config.js
+++ final-pjt-front/vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports = {
+  chainWebpack: (config) => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap((options) => {
+        options.compiler = require("vue-template-babel-compiler");
+        return options;
+      });
+  },
+};
